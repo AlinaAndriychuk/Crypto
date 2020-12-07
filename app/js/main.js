f61f665f20016bf90004f313620a171c719d1e49
AlinaAndriychuk <alinka.andriychuk2002@gmail.com>
2020-12-07 18:56:42 +0200

    container

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,14 @@ $(function() {
   //   $(".header__menu").toggleClass("open-menu");
   // });
 
-  // $(".header__link-item").on("click", function(event) {
-  //   let anchor = $(this);
-  //   $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top - 30 + "px"}, 777);
-  //   $(".header__menu").removeClass("open-menu");
+  $(".header__link-item").on("click", function(event) {
+    let anchor = $(this);
+    $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top + "px"}, 777);
+    $(".header__menu").removeClass("open-menu");
 
-  //   event.preventDefault();
-  //   return false;
-  // });
+    event.preventDefault();
+    return false;
+  });
 
   let xhttp = new XMLHttpRequest();
 
